8cb3077fba3cd98d68783222cc57a6487f76c037
Raja Vyshnavi Sriramoju <rvsriramoju@lbl.gov>
2024-12-02 18:33:41 +0400

    WIP: The gb parse function has an import issue

--- tiled/config/custom/gb.py
+++ tiled/config/custom/gb.py
@@ -2,6 +2,16 @@ import numpy as np
 from tiled.adapters.array import ArrayAdapter
 from tiled.structures.core import Spec
 from tiled.utils import path_from_uri
+#from tiled.config.custom.edf import parse_txt_accompanying_edf
+import pathlib
+import os
+import logging
+from logging import StreamHandler
+import fabio
+
+logger = logging.getLogger("tiled.adapters.edf")
+logger.addHandler(StreamHandler())
+logger.setLevel("INFO")
 
 
 def read(data_uri, structure=None, metadata=None, specs=None, access_policy=None):
@@ -24,3 +34,94 @@ def read(data_uri, structure=None, metadata=None, specs=None, access_policy=None
     array = data.reshape((pixels_y, pixels_x))
 
     return ArrayAdapter.from_array(array, metadata=metadata, specs=[Spec("gb")])
+
+def combine_edf_metadata_for_gb(hi_dict, lo_dict):
+    """Combine two dictionaries into one.
+
+    Parameters
+    ----------
+    hi_dict: dict
+        Dictionary containing metadata from the hi .edf file.
+    lo_dict: dict
+        Dictionary containing metadata from the lo .edf file
+    """
+    combined_dict = dict()
+
+    # get all the unique keys from both dictionaries
+    combined_keys = set(hi_dict.keys()).union(set(lo_dict.keys()))
+
+    # check if the values match for the same key in both dictionaries
+    for key in combined_keys:
+        hi_val = hi_dict.get(key)
+        lo_val = lo_dict.get(key)
+
+        if hi_val == lo_val:
+            # If values are the same, keep one entry
+            combined_dict[key] = hi_val
+        else:
+            # If values are different, add both with distinct keys
+            if hi_val is not None:
+                combined_dict[f"{key}_hi"] = hi_val
+            if lo_val is not None:
+                combined_dict[f"{key}_lo"] = lo_val
+
+    return combined_dict
+
+
+def parse_edf_accompanying_gb(file_path):
+    """Parse a .edf file produced at ALS beamline 7.3.3 into a dictionary.
+
+    Parameters
+    ----------
+    file_path: str or pathlib.Path
+        Filepath of the .edf file.
+    """
+
+    # Generate the hi edf file path
+    edf_hi_filepath = None
+    if isinstance(file_path, str):
+        edf_hi_filepath = file_path.replace("sfloat_2m.gb", "hi_2m.edf")
+    if isinstance(file_path, pathlib.Path):
+        edf_hi_filepath = file_path.with_suffix(".edf")
+        edf_hi_filepath = edf_hi_filepath.replace("sfloat", "hi")
+
+    # File does not exist, return empty dictionary
+    if not os.path.isfile(edf_hi_filepath):
+        return dict()
+    
+    #extract the header information and import the function for edf and read the metadata
+    # take the metadata from both and if the same key is present in both, keep the values of both   
+    # but index it with hi and lo
+
+    hi_file = fabio.open(edf_hi_filepath)
+    hi_header = hi_file.header
+    hi_date = hi_header.get("Date")
+    
+    edf_hi_metadata_dict = parse_txt_accompanying_edf(edf_hi_filepath)
+
+    edf_lo_filepath = None
+    if isinstance(file_path, str):
+        edf_lo_filepath = file_path.replace("sfloat_2m.gb", "lo_2m.edf")
+    if isinstance(file_path, pathlib.Path):
+        edf_lo_filepath = file_path.with_suffix(".edf")
+        edf_lo_filepath = edf_lo_filepath.replace("sfloat", "lo")
+
+    # File does not exist, return empty dictionary
+    if not os.path.isfile(edf_lo_filepath):
+        return dict()
+    
+    lo_file = fabio.open(edf_lo_filepath)
+    lo_header = lo_file.header
+    lo_date = lo_header.get("Date")
+    
+    edf_lo_metadata_dict = parse_txt_accompanying_edf(edf_lo_filepath)
+
+    # Combine the metadata dictionaries
+    gb_dictionary = combine_edf_metadata_for_gb(edf_hi_metadata_dict, edf_lo_metadata_dict)
+    print(gb_dictionary)
+
+    # Compare two dates and select the later one
+    gb_dictionary["Date"] = hi_date if hi_date > lo_date else lo_date
+
+    return gb_dictionary
+
