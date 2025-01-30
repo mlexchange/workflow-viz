329f768ee2a84aa4f0053c98f6d5cf3dcffd4fc8
Raja Vyshnavi Sriramoju <rvsriramoju@lbl.gov>
2025-01-30 10:34:55 -0800

    powereshell script update for tiled_catalog_register.ps1

--- tiled/tiled_catalog_register.ps1
+++ tiled/tiled_catalog_register.ps1
@@ -7,8 +7,8 @@ Write-Host "Data catalog for raw data: $Env:PATH_TO_RAW_DATA_CATALOG"
 Write-Host "Data catalog for processed data: $Env:PATH_TO_PROCESSED_DATA_CATALOG"
 
 # Should no longer be needed since tiled serve comes first
-if (-not ([string]::IsNullOrWhiteSpace($Env:PATH_TO_DATA_CATALOG)) -and -not (Test-Path "$Env:PATH_TO_DATA_CATALOG")) {
-    tiled catalog init "$Env:PATH_TO_DATA_CATALOG"
+if (-not ([string]::IsNullOrWhiteSpace($Env:PATH_TO_RAW_DATA_CATALOG)) -and -not (Test-Path "$Env:PATH_TO_RAW_DATA_CATALOG")) {
+    tiled catalog init "$Env:PATH_TO_RAW_DATA_CATALOG"
 }
 
 ## Will overwrite
@@ -24,5 +24,28 @@ if (-not ([string]::IsNullOrWhiteSpace("$Env:PATH_TO_RAW_DATA")) -and (Test-Path
     "$Env:PATH_TO_RAW_DATA"
 } else {
     Write-Host "The directory for raw data ($Env:PATH_TO_RAW_DATA) does not exist."
+}
+
+
+# Should no longer be needed since tiled serve comes first
+if (-not ([string]::IsNullOrWhiteSpace($Env:PATH_TO_PROCESSED_DATA_CATALOG)) -and -not (Test-Path "$Env:PATH_TO_PROCESSED_DATA_CATALOG")) {
+    tiled catalog init "$Env:PATH_TO_PROCESSED_DATA_CATALOG"
+}
+
+## Will overwrite
+if (-not ([string]::IsNullOrWhiteSpace("$Env:PATH_TO_PROCESSED_DATA")) -and (Test-Path "$Env:PATH_TO_PROCESSED_DATA" -PathType Container)){
+    tiled register $Env:TILED_URI --verbose `
+    --prefix "/processed" `
+    --ext '.edf=application/x-edf' `
+    --adapter 'application/x-edf=custom.edf:read' `
+    --ext '.gb=application/x-gb' `
+    --adapter 'application/x-gb=custom.gb:read' `
+    --ext '.cbf=application/x-cbf' `
+    --adapter 'application/x-cbf=custom.cbf:read' `
+    --walker 'custom.lambda_nxs:walk' `
+    --adapter 'multipart/related;type=application/x-hdf5=custom.lambda_nxs:read_sequence' `
+    "$Env:PATH_TO_PROCESSED_DATA"
+} else {
+    Write-Host "The directory for processed data ($Env:PATH_TO_PROCESSED_DATA) does not exist."
 }
     
