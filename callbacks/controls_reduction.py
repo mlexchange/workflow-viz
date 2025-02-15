ed791128551018f29bb5e564469e2644a3adb817
Wiebke Köpp <wkoepp@lbl.gov>
2025-02-14 17:30:21 -0800

    Call different flow depending on experiment type
    horizontal cut for grazing incidence and azimuthal integration for transmission

--- callbacks/controls_reduction.py
+++ callbacks/controls_reduction.py
@@ -11,6 +11,7 @@ from utils.prefect import get_full_deployment_names, schedule_prefect_flow
     State("scan-name-uri-map", "data"),
     State("mask-name", "value"),
     State("mask-name-uri-map", "data"),
+    State("experiment-type", "value"),
     State("beamcenter-x", "value"),
     State("beamcenter-y", "value"),
     State("wavelength", "value"),
@@ -24,6 +25,10 @@ from utils.prefect import get_full_deployment_names, schedule_prefect_flow
     State("radial-range-max", "value"),
     State("azimuthal-range-min", "value"),
     State("azimuthal-range-max", "value"),
+    State("horizontal-cut-half-width", "value"),
+    State("horizontal-cut-pos-y", "value"),
+    State("horizontal-x-min", "value"),
+    State("horizontal-x-max", "value"),
     prevent_initial_call=True,
 )
 def submit_reduction_to_compute(
@@ -32,6 +37,7 @@ def submit_reduction_to_compute(
     scan_name_uri_mapper,
     mask_name,
     mask_name_uri_mapper,
+    experiment_type,
     beamcenter_x,
     beamcenter_y,
     wavelength,
@@ -45,29 +51,54 @@ def submit_reduction_to_compute(
     radial_range_max,
     azimuthal_range_min,
     azimuthal_range_max,
+    horizontal_cut_half_width,
+    horizontal_cut_pos_y,
+    horizontal_cut_x_min,
+    horizontal_x_max,
 ):
     if n_clicks:
         scan_uri = scan_name_uri_mapper[scan_name]
         mask_uri = mask_name_uri_mapper[mask_name]
-        parameters = {
+        parameters_calibration = {
             "input_uri_data": scan_uri,
             "input_uri_mask": mask_uri,
             "beamcenter_x": beamcenter_x,
             "beamcenter_y": beamcenter_y,
             "wavelength": wavelength,
-            "polarization_factor": 0.99,
             "sample_detector_dist": sample_detector_dist,
             "pix_size": pix_size,
-            "rotation": detector_rotation,
-            "tilt": detector_tilt,
-            "num_bins": num_bins_integration,
-            "chi_min": azimuthal_range_min,
-            "chi_max": azimuthal_range_max,
-            "inner_radius": radial_range_min,
-            "outer_radius": radial_range_max,
             "output_unit": "q",  # "q"
         }
-        flow_name = "integration-azimuthal"
+        if experiment_type is not None and experiment_type.startswith("GI"):
+            parameters_cut = {
+                "cut_half_width": horizontal_cut_half_width,
+                "cut_pos_y": horizontal_cut_pos_y,
+                "x_min": horizontal_cut_x_min,
+                "x_max": horizontal_x_max,
+            }
+            parameters = {
+                **parameters_calibration,
+                **parameters_cut,
+                "incident_angle": incident_angle,
+            }
+            flow_name = "horizontal-cut"
+        else:
+            parameters_integration = {
+                "num_bins": num_bins_integration,
+                "chi_min": azimuthal_range_min,
+                "chi_max": azimuthal_range_max,
+                "inner_radius": radial_range_min,
+                "outer_radius": radial_range_max,
+            }
+            parameters = {
+                **parameters_calibration,
+                **parameters_integration,
+                "polarization_factor": 0.99,
+                "rotation": detector_rotation,
+                "tilt": detector_tilt,
+            }
+            flow_name = "integration-azimuthal"
+
         reduction_flows = get_full_deployment_names()
         deployment_name = reduction_flows[flow_name]
         flow_run_id = schedule_prefect_flow(deployment_name, parameters)
