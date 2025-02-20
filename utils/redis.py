6921e9ef81403a24abf5f6be7fb2946a9c94bf56
Dylan McReynolds <dmcreynolds@lbl.gov>
2025-02-19 20:06:22 -0500

    change to syncronous redis

--- utils/redis.py
+++ utils/redis.py
@@ -1,45 +1,45 @@
 import json
 import os
 
-import redis.asyncio as redis
+import redis as redis
 
 
 class RedisConn:
     def __init__(self, redis_conn: redis.Redis):
         self.redis_conn = redis_conn
 
-    async def get(self, key: str):
-        return await self.redis_conn.get(key)
+    def get(self, key: str):
+        return self.redis_conn.get(key)
 
-    async def set(self, key: str, value):
-        await self.redis_conn.set(key, value)
+    def set(self, key: str, value):
+        self.redis_conn.set(key, value)
 
-    async def get_json(self, key: str):
-        value_s = await self.redis_conn.get(key)
+    def get_json(self, key: str):
+        value_s = self.redis_conn.get(key)
         if value_s:
             values_js = json.loads(value_s)
         else:
             values_js = {}
         return values_js
 
-    async def set_json(self, key: str, value: dict):
+    def set_json(self, key: str, value: dict):
         value_s = json.dumps(value)
-        await self.reddis_conn.set(key, value_s)
+        self.reddis_conn.set(key, value_s)
 
-    async def redis_subscribe(self, channel_name: str, callback: callable) -> None:
+    def redis_subscribe(self, channel_name: str, callback: callable) -> None:
         """Listens for messages on a Redis Pub/Sub channel asynchronously."""
         pubsub = self.redis_conn.pubsub()
         channel_name = "scattering"
-        await pubsub.subscribe(channel_name)  # Subscribe to the channel
+        pubsub.subscribe(channel_name)  # Subscribe to the channel
 
         print(f"Listening for messages on '{channel_name}'...")
 
-        async for message in pubsub.listen():
+        for message in pubsub.listen():
             if (
                 message["type"] == "message"
             ):  # Ignore subscription confirmation messages
                 if message["channel"] == channel_name:
-                    await callback(message["data"])
+                    callback(message["data"])
 
     @classmethod
     def from_settings(cls, settings: dict) -> "RedisConn":
